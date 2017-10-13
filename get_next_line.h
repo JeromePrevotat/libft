5ac487299ef836f2f1ce6512b6a9c160237fcc70
Jerome PREVOTAT <jprevota@e1r2p23.42.fr>
2017-10-13 15:04:22 +0200

    gnl added

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jprevota <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/28 16:44:38 by jprevota          #+#    #+#             */
+/*   Updated: 2017/10/10 14:23:33 by jprevota         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 10
+# include "./libft.h"
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+int		get_next_line(const int fd, char **line);
+
+#endif
